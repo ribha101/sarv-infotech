e47f413f5d10d307dc67c753ba24fb27736603c1
ribha101 <kribha101@gmail.com>
2026-09-13 12:31:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,75 +1,24 @@
-let taskInput = document.getElementById("taskInput");
-let addTask = document.getElementById("addTask");
-let taskList = document.getElementById("taskList");
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
 
-// Page load hone par saved tasks dikhana
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+buttons.forEach(function(button) {
 
-tasks.forEach(function(task) {
-    createTask(task);
-});
+    button.addEventListener("click", function() {
 
-// Add Task
-addTask.addEventListener("click", function() {
+        let value = button.textContent;
 
-    let task = taskInput.value.trim();
-
-    if (task !== "") {
-        createTask(task);
-
-        tasks.push(task);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-
-        taskInput.value = "";
-    }
-});
-
-// Task create karne ka function
-function createTask(task) {
-
-    let li = document.createElement("li");
-
-    li.textContent = task;
-
-    // Edit Button
-    let editButton = document.createElement("button");
-    editButton.textContent = "Edit";
-
-    // Delete Button
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-
-    li.appendChild(editButton);
-    li.appendChild(deleteButton);
-
-    // Edit
-    editButton.addEventListener("click", function() {
-
-        let newTask = prompt("Edit your task:", task);
-
-        if (newTask !== null && newTask.trim() !== "") {
-            li.firstChild.textContent = newTask;
-            task = newTask;
-
-            tasks = Array.from(taskList.children).map(function(item) {
-                return item.firstChild.textContent;
-            });
-
-            localStorage.setItem("tasks", JSON.stringify(tasks));
+        if (value === "C") {
+            display.value = "";
         }
+
+        else if (value === "=") {
+            display.value = eval(display.value);
+        }
+
+        else {
+            display.value += value;
+        }
+
     });
 
-    // Delete
-    deleteButton.addEventListener("click", function() {
-
-        li.remove();
-
-        tasks = Array.from(taskList.children).map(function(item) {
-            return item.firstChild.textContent;
-        });
-
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
-
-    taskList.appendChild(li);
-}
+});
